3095f565878d0bf6614ddd8f7bc8edb8e4fc6cef
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2021-06-20 11:06:06 +0300

    Delete Main.java

--- duel/src/main/java/ru/obukhova/vita/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.obukhova.vita;
-
-public class Main {
-    public static void main(String[] args) {
-        // make context-bean-maker
-        // create Game, Game fills with the beans of Human and Bot players
-        // then start game
-    }
-}
